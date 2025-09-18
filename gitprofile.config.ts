b9c95b1a0fb062f4339d34ac714980c5e8cd2931
Aman Gupta <4programmimg@gmail.com>
2025-09-18 20:05:29 +0530

    good enough

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -14,7 +14,7 @@ const CONFIG = {
     github: {
       display: true, // Display GitHub projects?
       header: 'Github Projects',
-      mode: 'automatic', // Mode can be: 'automatic' or 'manual'
+      mode: 'manual', // Mode can be: 'automatic' or 'manual'
       automatic: {
         sortBy: 'stars', // Sort projects by 'stars' or 'updated'
         limit: 4, // How many projects to display.
@@ -113,7 +113,7 @@ const CONFIG = {
       name: 'CI/CD for Machine Learning',
       body: 'Datacamp Team',
       year: '2025',
-      link: 'Datacamp.com',
+      link: '',
     },
   ],
   educations: [
